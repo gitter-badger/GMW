d9a8ab5616ec2419d0877182ec3c7fa2cb3f9a18
Hein Haraldson Berg <hein@keyteq.no>
2012-07-15 20:22:28 +0200

    Set sensor=true, for some geolocation magic?

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 		<title>Google Maps Wrapper</title>
 		<script src="//code.jquery.com/jquery-latest.min.js"></script>
-		<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAGGoUbt_msAheJrKfXgcLIamH6NRcA5ak&sensor=SET_TO_TRUE_OR_FALSE"></script>
+		<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAGGoUbt_msAheJrKfXgcLIamH6NRcA5ak&sensor=true"></script>
 	</head>
 	<body>
     	<div id="google-map-canvas"></div>
